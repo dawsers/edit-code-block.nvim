5e4e31012eafa113216cb5894f696682833f8e7f
dawsers <dawser@gmx.com>
2023-05-10 00:52:33 +0200

    Add EditCodeBlockSelection and inline editing

--- lua/ecb.lua
+++ lua/ecb.lua
@@ -6,19 +6,29 @@ local create_commands = function()
     function(opts) require('ecb').edit_code_block(opts) end,
     {
       nargs = '*',
-      complete = function(ArgLead, CmdLine, cursorPos)
+      complete = function(_, _, _)
         return { "split", "vsplit", "tabnew", "rightbelow", "leftabove" }
       end,
       desc = 'edit embedded code block in new window'
     })
-  vim.api.nvim_create_user_command('EditOrgCodeBlock',
-    function(opts) require('ecb').edit_org_code_block(opts) end,
+  vim.api.nvim_create_user_command('EditCodeBlockOrg',
+    function(opts) require('ecb').edit_code_block_org(opts) end,
     {
       nargs = '*',
-      complete = function(ArgLead, CmdLine, cursorPos)
+      complete = function(_, _, _)
         return { "split", "vsplit", "tabnew", "rightbelow", "leftabove" }
       end,
       desc = 'edit embedded org mode code block in new window'
+    })
+  vim.api.nvim_create_user_command('EditCodeBlockSelection',
+    function(opts) require('ecb').edit_code_block_selection(opts) end,
+    {
+      nargs = '+',
+      complete = function(_, _, _)
+        return { "split", "vsplit", "tabnew", "rightbelow", "leftabove" }
+      end,
+      desc = 'edit selected code in new window',
+      range = true
     })
 end
 
@@ -29,9 +39,23 @@ M.setup = function(opts)
   end
 end
 
-local function create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, filetype)
+local function create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, ecol, filetype)
   local mwin = vim.api.nvim_get_current_win()
-  local lines = vim.api.nvim_buf_get_lines(mdbufnr, srow, erow, false)
+  local lines
+  if ecol ~= 0 then
+    lines = vim.api.nvim_buf_get_lines(mdbufnr, srow, erow + 1, false)
+  else
+    lines = vim.api.nvim_buf_get_lines(mdbufnr, srow, erow, false)
+  end
+  local pre, post
+  if erow - srow + 1 <= #lines then
+    post = string.sub(lines[erow - srow + 1], ecol + 1)
+    lines[erow - srow + 1] = string.sub(lines[erow - srow + 1], 1, ecol)
+  end
+  if scol > 0 then
+    pre = string.sub(lines[1], 1, scol)
+    lines[1] = string.sub(lines[1], scol + 1)
+  end
   vim.cmd(win_cmd)
   local win = vim.api.nvim_get_current_win()
   local bufnr = vim.api.nvim_create_buf(true, false)
@@ -49,7 +73,14 @@ local function create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, 
   vim.api.nvim_buf_set_option(bufnr, 'modified', false)
   vim.api.nvim_win_set_buf(win, bufnr)
   -- Calculate cursor position in new window
-  vim.api.nvim_win_set_cursor(win, { row - srow, col })
+  local crow = row - srow
+  local ccol
+  if crow > 1 then
+    ccol = col
+  else
+    ccol = col - scol
+  end
+  vim.api.nvim_win_set_cursor(win, { crow, ccol })
 
   -- Auto commands to update main buffer
   vim.api.nvim_create_autocmd({'BufWrite', 'BufWriteCmd'}, {
@@ -57,8 +88,24 @@ local function create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, 
     callback = function()
       local crow, ccol = unpack(vim.api.nvim_win_get_cursor(win))
       local nlines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
-      vim.api.nvim_buf_set_lines(mdbufnr, srow, erow, true, nlines)
-      vim.api.nvim_win_set_cursor(mwin, { crow + srow, ccol })
+      if #nlines > 0 and pre then
+        nlines[1] = pre .. nlines[1]
+      end
+      if #nlines > 0 and post then
+        nlines[#nlines] = nlines[#nlines] .. post
+      end
+      if ecol ~= 0 then
+        vim.api.nvim_buf_set_lines(mdbufnr, srow, erow + 1, true, nlines)
+      else
+        vim.api.nvim_buf_set_lines(mdbufnr, srow, erow, true, nlines)
+      end
+      local mcol
+      if crow > 1 then
+        mcol = ccol
+      else
+        mcol = ccol + scol
+      end
+      vim.api.nvim_win_set_cursor(mwin, { crow + srow, mcol })
       -- Set as not modified
       vim.api.nvim_buf_set_option(bufnr, 'modified', false)
     end,
@@ -88,9 +135,8 @@ M.edit_code_block = function (opts)
   -- Ensure the file gets parsed. Neovim doesn't re-parse the file on changes
   -- unless the tree-sitter highlighter is enabled
   file_parser:parse()
-  
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local range = { row - 1, col, row - 1, col }
+  local range = { row - 1, col, row - 1, col + 1 }
   local ltree = file_parser:language_for_range(range)
   if not ltree then
     return
@@ -101,13 +147,13 @@ M.edit_code_block = function (opts)
     local node = tree:root()
     if vim.treesitter.node_contains(node, range) then
       local mdbufnr = vim.fn.bufnr()
-      local srow, scol, erow, _ = node:range(false)
-      create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, ltree:lang())
+      local srow, scol, erow, ecol = node:range(false)
+      create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, ecol, ltree:lang())
     end
   end
 end
 
-M.edit_org_code_block = function (opts)
+M.edit_code_block_org = function (opts)
   local win_cmd
   if opts and opts.args and opts.args ~= '' then
     win_cmd = opts.args
@@ -121,7 +167,6 @@ M.edit_org_code_block = function (opts)
   -- Ensure the file gets parsed. Neovim doesn't re-parse the file on changes
   -- unless the tree-sitter highlighter is enabled
   file_parser:parse()
-  
   local node = vim.treesitter.get_node()
   if not node then
     return
@@ -159,10 +204,34 @@ M.edit_org_code_block = function (opts)
       if contents then
         srow, scol, erow, ecol = contents:range(false)
         local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-        create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, language)
+        -- The cursor may belong to the block but not the contents, adjust
+        if erow < row then
+          row = erow
+        end
+        create_edit_buffer(win_cmd, mdbufnr, row, col, srow, scol, erow, ecol, language)
       end
     end
   end
+end
+
+M.edit_code_block_selection = function (opts)
+  if not opts or not opts.args or opts.args == '' then
+    error("You need to specify the file type of the selection")
+  end
+  local filetype = opts.fargs[1]
+  local win_cmd
+  if #opts.fargs > 1 then
+    win_cmd = table.concat(opts.fargs, ' ', 2)
+  else
+    win_cmd = wincmd
+  end
+
+  local _, srow, scol, _ = unpack(vim.fn.getcharpos("'<"))
+  local _, erow, ecol, _ = unpack(vim.fn.getcharpos("'>"))
+
+  local mdbufnr = vim.fn.bufnr()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  create_edit_buffer(win_cmd, mdbufnr, row, col, srow - 1, scol - 1, erow - 1, ecol, filetype)
 end
 
 return M
